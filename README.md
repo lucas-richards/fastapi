851a1b9a5617330a4a8fd12045667892b047dd67
lucas richards dev <lucasrichardsdev@gmail.com>
2024-01-10 10:36:06 -0800

    query

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 
 # Getting started
 
+
+
+
 ## Requirements¶
  - Python 3.8+
 
@@ -9,12 +12,34 @@ FastAPI stands on the shoulders of giants:
  - Starlette for the web parts.
  - Pydantic for the data parts.
 
-## Run app
+## Run server
 
+This examples need the pipenv shell activated to work -> run: $ pipenv shell (to activate the env)
+
+Then cd into the example you want to run
+
+Then run the server
 
 ```console
 $ uvicorn main:app --reload 
 ```
-% --reload is to restart the server every time something changes 
+<!-- --reload is to restart the server every time something changes  -->
 
 ## Interactive API docs
+
+### docs
+
+http://127.0.0.1:8000/docs.
+
+You will see the automatic interactive API documentation (provided by Swagger UI):
+
+### redocs
+
+http://127.0.0.1:8000/redoc.
+
+You will see the alternative automatic documentation (provided by ReDoc):
+
+## Tutorial
+
+ - Basic: https://fastapi.tiangolo.com/tutorial/
+ - Advanced: 
---
+++ tutorial/main.py
@@ -0,0 +1,51 @@
+from enum import Enum
+from fastapi import FastAPI
+
+app = FastAPI()
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+fake_items_db = [
+    {"item_name": "Foo"}, 
+    {"item_name": "Bar"}, 
+    {"item_name": "Baz"}
+]
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/") # Define a path operation decorator(@)
+async def root(): # Define a path operation function
+    return {"message": "Hello World"}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
